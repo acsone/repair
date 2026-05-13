ee4fd5877329f1c7e99353769a1a09d6c8ea7291
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-05-13 12:32:26 +0200

    [PERF] repair_stock_consumption_step: add partial index on stock_move.repair_id

--- repair_stock_consumption_step/models/stock_move.py
+++ repair_stock_consumption_step/models/stock_move.py
@@ -5,8 +5,23 @@ from odoo import models
 
 
 class StockMove(models.Model):
-
     _inherit = "stock.move"
+
+    def init(self):
+        """
+        Add a partial index on repair_id to optimize filtering.
+        Standard Odoo does not index this field in stock.move.
+        """
+        res = super().init()
+        index_name = "stock_move_repair_id_partial_index"
+        query = f"""
+            CREATE INDEX IF NOT EXISTS {index_name}
+            ON stock_move (repair_id)
+            WHERE repair_id IS NOT NULL
+            """
+        # pylint: disable=sql-injection
+        self.env.cr.execute(query)
+        return res
 
     def _action_done(self, cancel_backorder=False):
         repair_moves = self.browse()
